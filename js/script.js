5b7dc48fbc44c4118640b03df34174c1829fd6e5
oshaniweerakoon04-coder <oshaniweerakoon04@gmail.com>
2026-08-30 12:05:33 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const watches = {
+
+    eclipse: {
+        name: "Ruwanpura Eclipse",
+        description: "A bold automatic timepiece with a deep black dial.",
+        model: "models/Eclipse.glb"
+    },
+
+    royal: {
+        name: "Ruwanpura Royal",
+        description: "Inspired by timeless elegance.",
+        model: "models/Royal.glb"
+    },
+
+    chrono: {
+        name: "Ruwanpura Chrono",
+        description: "Designed for modern adventures.",
+        model: "models/Chrono.glb"
+    }
+
+};
